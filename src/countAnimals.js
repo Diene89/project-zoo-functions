70c42a42a71f44ce67c5448f0eb0e2602b55623c
Diene <email.da.diene@gmail.com>
2022-01-26 16:04:31 -0300

    Requisito 5 OK

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// Com a ajuda do Pedro Fideles e Luiz Tharik
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      const key = curr.name;
+      const value = curr.residents.length;
+      acc[key] = value;
+      return acc;
+    }, {});
+  }
+  const foundAnimal = species.find((specie) => specie.name === animal.specie);
+  if (animal.sex) {
+    return foundAnimal.residents.filter((resident) => resident.sex === animal.sex).length;
+  }
+  return foundAnimal.residents.length;
 }
 
 module.exports = countAnimals;
