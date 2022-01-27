e2acec1f1a2ce587f4e244539279b5972796c49e
Diene <email.da.diene@gmail.com>
2022-01-27 13:38:04 -0300

    Requisito 9 Ok

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeData = employees.find((employee) => employee.id === id);
+  const firstSpecie = species.find((specie) => specie.id === employeeData.responsibleFor[0]);
+  const oldSpecie = firstSpecie.residents.sort((specie1, specie2) => specie2.age - specie1.age);
+  return Object.values(oldSpecie[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
